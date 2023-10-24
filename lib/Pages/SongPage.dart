dd451a1b91dde95afa65ca600f1b09b29e1183a3
Nitish Kumar <79355266+MrNitishroy@users.noreply.github.com>
2023-10-24 16:28:13 +0530

    first commit

---
+++ lib/Pages/SongPage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SongPage extends StatelessWidget {
+  const SongPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
